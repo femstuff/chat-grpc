b2e51dfaf2b4b5df9729d482b598b8b0c2fe92e8
Тихон Савенков <tihon.savenckov@yandex.ru>
2025-03-21 15:08:37 +0500

    data validation

--- Auth-service/internal/utils.go
+++ Auth-service/internal/utils.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"errors"
 	"time"
 
 	"chat-grpc/proto_gen"
@@ -13,4 +14,20 @@ func ConvertTimeStamp(t *proto_gen.Timestamp) string {
 
 	tConv := time.Unix(t.Seconds, int64(t.Nanos))
 	return tConv.Format("2006-01-02 15:04:05")
+}
+
+func Validate(name, email, pass string) error {
+	if name == "" {
+		return errors.New("name field cannot be empty")
+	}
+
+	if email == "" {
+		return errors.New("email field cannot be empty")
+	}
+
+	if len(pass) < 4 {
+		return errors.New("pass must be more than 4 char")
+	}
+
+	return nil
 }
