b9758fe257e8a4b00749cebebbb42e7b7ceadde2
Тихон Савенков <tihon.savenckov@yandex.ru>
2025-03-25 11:35:40 +0500

    all handlers with logger

--- Auth-service/internal/repository/db.go
+++ Auth-service/internal/repository/db.go
@@ -3,22 +3,25 @@ package repository
 import (
 	"database/sql"
 	"fmt"
-	"log"
+
+	"go.uber.org/zap"
 )
 
-func NewDb() (*sql.DB, error) {
+func NewDb(log *zap.Logger) (*sql.DB, error) {
 	dsn := "host=localhost user=postgres password=1111 dbname=authdb sslmode=disable"
 
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
+		log.Error("Failed connecting to db", zap.Error(err))
 		return nil, fmt.Errorf("error connecting to db: %w", err)
 	}
 
 	if err = db.Ping(); err != nil {
+		log.Error("Failed pinging db", zap.Error(err))
 		return nil, fmt.Errorf("error pinging db: %w", err)
 	}
 
-	log.Println("Connected to DB successfully")
+	log.Info("Connected to DB successfully")
 
 	query := `
 	CREATE TABLE IF NOT EXISTS users (
@@ -33,10 +36,11 @@ func NewDb() (*sql.DB, error) {
 
 	_, err = db.Exec(query)
 	if err != nil {
+		log.Error("Failed creating table", zap.Error(err))
 		return nil, fmt.Errorf("error creating table: %w", err)
 	}
 
-	log.Println("Table 'users' is ready")
+	log.Info("Table users is ready")
 
 	return db, nil
 }
