f1a0ddd9e0b421f96d7fa07e8848bcbace394ed1
Тихон Савенков <tihon.savenckov@yandex.ru>
2025-04-25 15:33:10 +0500

    fix migrations

--- migrations/5_create_chat_users_table.up.sql
+++ migrations/5_create_chat_users_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS chat_users (
-  chat_id INT NOT NULL REFERENCES chats(id) ON DELETE CASCADE,
-    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    chat_id INT NOT NULL REFERENCES chats(id) ON DELETE CASCADE,
+    user_id INT NOT NULL,
     PRIMARY KEY (chat_id, user_id)
 );
